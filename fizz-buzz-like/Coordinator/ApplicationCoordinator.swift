97e57cc8a940d780bcceda9d6ef1ba2d46233234
Guillaume Berthier <guillaume.berthier@fabernovel.com>
2021-07-05 11:27:52 +0200

    Use ApplicationCoordinator instead of storyboard

---
+++ fizz-buzz-like/Coordinator/ApplicationCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  ApplicationCoordinator.swift
+//  fizz-buzz-like
+//
+//  Created by Guillaume Berthier on 05/07/2021.
+//
+
+import Foundation
+import UIKit
+
+class ApplicationCoordinator {
+
+    private let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start() {
+        // TODO (Guillaume Berthier) Display FormViewController
+    }
+}

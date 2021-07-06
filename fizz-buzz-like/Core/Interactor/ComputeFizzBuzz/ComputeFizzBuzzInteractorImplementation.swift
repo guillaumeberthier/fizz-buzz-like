ef06fb82d880a7a0542e177e7f29bba5a5d369c3
Guillaume Berthier <guillaume.berthier@fabernovel.com>
2021-07-06 16:54:59 +0200

    Compute on global thread

--- fizz-buzz-like/Core/Interactor/ComputeFizzBuzz/ComputeFizzBuzzInteractorImplementation.swift
+++ fizz-buzz-like/Core/Interactor/ComputeFizzBuzz/ComputeFizzBuzzInteractorImplementation.swift
@@ -13,31 +13,33 @@ class ComputeFizzBuzzInteractorImplementation: ComputeFizzBuzzInteractor {
 
     func execute(request: FizzBuzzRequest,
                  completion: @escaping (Result<FizzBuzzResponse, AppError>) -> Void) {
-        guard request.isValid else {
-            completion(.failure(.invalidRequest))
-            return
+        DispatchQueue.global().async {
+            guard request.isValid else {
+                DispatchQueue.main.async { completion(.failure(.invalidRequest)) }
+                return
+            }
+            // TODO (Guillaume Berthier) Register request to handle statistics
+            var words: [String] = []
+            for i in 1...request.limit {
+                var word = String(i)
+                var isMultipleOfFirst = false
+                var isMultipleOfSecond = false
+                if i.isMultiple(of: request.firstDivider) {
+                    word = request.firstText
+                    isMultipleOfFirst = true
+                }
+                if i.isMultiple(of: request.secondDivider) {
+                    word = request.secondText
+                    isMultipleOfSecond = true
+                }
+                if isMultipleOfFirst && isMultipleOfSecond {
+                    word = request.firstText + request.secondText
+                }
+                words.append(word)
+            }
+            let response = FizzBuzzResponse(words: words)
+            DispatchQueue.main.async { completion(.success(response)) }
         }
-        // TODO (Guillaume Berthier) Register request to handle statistics
-        var words: [String] = []
-        for i in 1...request.limit {
-            var word = String(i)
-            var isMultipleOfFirst = false
-            var isMultipleOfSecond = false
-            if i.isMultiple(of: request.firstDivider) {
-                word = request.firstText
-                isMultipleOfFirst = true
-            }
-            if i.isMultiple(of: request.secondDivider) {
-                word = request.secondText
-                isMultipleOfSecond = true
-            }
-            if isMultipleOfFirst && isMultipleOfSecond {
-                word = request.firstText + request.secondText
-            }
-            words.append(word)
-        }
-        let response = FizzBuzzResponse(words: words)
-        completion(.success(response))
     }
 }
 

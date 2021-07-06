ec9638dd1a99a64f2da4f6639a81ffefbbef4c9b
Guillaume Berthier <guillaume.berthier@fabernovel.com>
2021-07-06 16:09:41 +0200

    Add ComputationResultViewModel and its mapper

---
+++ fizz-buzz-like/App/ComputationResult/ViewModel/ComputationResultViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ComputationResultViewModel.swift
+//  fizz-buzz-like
+//
+//  Created by Guillaume Berthier on 06/07/2021.
+//
+
+import Foundation
+
+/// Textual representation of the fizz buzz result
+struct ComputationResultViewModel {
+    let text: String
+}

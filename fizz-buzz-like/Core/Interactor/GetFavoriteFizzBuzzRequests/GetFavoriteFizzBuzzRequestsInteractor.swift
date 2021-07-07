be3cdfcb758efbb6d97c48469bc09f515b57ffe0
Guillaume Berthier <guillaume.berthier@fabernovel.com>
2021-07-07 17:37:10 +0200

    Add interactor statistics related tests

--- fizz-buzz-like/Core/Interactor/GetFavoriteFizzBuzzRequests/GetFavoriteFizzBuzzRequestsInteractor.swift
+++ fizz-buzz-like/Core/Interactor/GetFavoriteFizzBuzzRequests/GetFavoriteFizzBuzzRequestsInteractor.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// Get count statistics (see *FizzBuzzRequestCountStatistics* for more detail)
 protocol GetFavoriteFizzBuzzRequestsInteractor {
     func execute(completion: (Result<FizzBuzzRequestCountStatistics, AppError>) -> Void)
 }
